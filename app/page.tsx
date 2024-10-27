747d822722e395dd500773699734fe1879573dca
frmnsyh19 <19.firmann@gmail.com>
2024-10-27 16:09:41 +0700

    updatecommit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-
+import './style.css'
 export default function Home() {
   return (
     <div className="w-full">
+      <div className=" w-full h-72" id="kocak"></div>
       <img src="/image/logonexus.png" alt="" className=" w-64 h-96" />
       <Image src={'/image/logonexus.png'} alt="logo" width={300} height={400} />
     </div>
